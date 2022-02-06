0858a333ccaddbd14d487817444a442c909e4bc2
BuildTools <44118666+hp3721@users.noreply.github.com>
2022-02-06 04:39:50 -0600

    Fix tweak toggle menu

--- src/main/java/com/red/masaadditions/tweakeroo_additions/tweakeroo_mixin/MixinGuiConfigs.java
+++ src/main/java/com/red/masaadditions/tweakeroo_additions/tweakeroo_mixin/MixinGuiConfigs.java
@@ -2,12 +2,8 @@ package com.red.masaadditions.tweakeroo_additions.tweakeroo_mixin;
 
 import com.google.common.collect.ImmutableList;
 import com.red.masaadditions.tweakeroo_additions.config.FeatureToggleExtended;
-import fi.dy.masa.malilib.config.ConfigType;
-import fi.dy.masa.malilib.config.ConfigUtils;
-import fi.dy.masa.malilib.config.IConfigBase;
-import fi.dy.masa.malilib.config.IConfigValue;
-import fi.dy.masa.malilib.gui.GuiConfigsBase;
-import fi.dy.masa.tweakeroo.config.Configs;
+import fi.dy.masa.malilib.config.*;
+import fi.dy.masa.malilib.config.options.BooleanHotkeyGuiWrapper;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.gui.GuiConfigs;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,21 +11,20 @@ import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 @Pseudo
 @Mixin(value = GuiConfigs.class, remap = false)
 public class MixinGuiConfigs {
-    private static final ImmutableList<IConfigValue> FEATURE_TOGGLES = new ImmutableList.Builder<IConfigValue>().addAll(Arrays.asList(FeatureToggle.values())).addAll(Arrays.asList(FeatureToggleExtended.values())).build();
+    private static final ImmutableList<IHotkeyTogglable> FEATURE_TOGGLES = new ImmutableList.Builder<IHotkeyTogglable>().addAll(Arrays.asList(FeatureToggle.values())).addAll(Arrays.asList(FeatureToggleExtended.values())).build();
+
+    protected BooleanHotkeyGuiWrapper wrapConfig(IHotkeyTogglable config) {
+        return new BooleanHotkeyGuiWrapper(config.getName(), config, config.getKeybind());
+    }
 
     @ModifyArg(method = "getConfigs", at = @At(value = "INVOKE", target = "Lfi/dy/masa/malilib/gui/GuiConfigsBase$ConfigOptionWrapper;createFor(Ljava/util/Collection;)Ljava/util/List;", ordinal = 1), index = 0)
     private Collection<? extends IConfigBase> ExtendTweaks(Collection<? extends IConfigBase> configs) {
-        List<IConfigBase> list = new ArrayList<>();
-        list.addAll(ConfigUtils.createConfigWrapperForType(ConfigType.BOOLEAN, FEATURE_TOGGLES));
-        list.addAll(ConfigUtils.createConfigWrapperForType(ConfigType.HOTKEY, FEATURE_TOGGLES));
-        return list;
+        return FEATURE_TOGGLES.stream().map(this::wrapConfig).toList();
     }
 }
