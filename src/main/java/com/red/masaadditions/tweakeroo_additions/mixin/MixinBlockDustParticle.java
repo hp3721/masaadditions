664716dc534178d1d939c690dca0518c42a68adb
BuildTools <44118666+hp3721@users.noreply.github.com>
2022-02-20 17:15:51 -0600

    Fixed insane block breaking particles

--- src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinBlockDustParticle.java
+++ src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinBlockDustParticle.java
@@ -2,7 +2,7 @@ package com.red.masaadditions.tweakeroo_additions.mixin;
 
 import com.red.masaadditions.tweakeroo_additions.config.FeatureToggleExtended;
 import net.minecraft.client.particle.BlockDustParticle;
-import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.SpriteBillboardParticle;
 import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,13 +10,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockDustParticle.class)
-public abstract class MixinBlockDustParticle extends Particle {
+public abstract class MixinBlockDustParticle extends SpriteBillboardParticle {
     protected MixinBlockDustParticle(ClientWorld worldIn, double posXIn, double posYIn, double posZIn) {
         super(worldIn, posXIn, posYIn, posZIn);
     }
 
     // From UsefulMod by nessie
-    @Inject(method = "<init>", at = @At("RETURN"))
+    @Inject(method = "<init>(Lnet/minecraft/client/world/ClientWorld;DDDDDDLnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)V", at = @At("RETURN"))
     private void removeRandomParticleMotion(CallbackInfo ci) {
         if (FeatureToggleExtended.TWEAK_INSANE_BLOCK_BREAKING_PARTICLES.getBooleanValue()) {
             final double multiplier = this.random.nextFloat() * 5;
