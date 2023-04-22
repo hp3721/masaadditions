a799a1e527bcf6e0a1d60df5de5fcbaa21ef3bf8
hp3721 <44118666+hp3721@users.noreply.github.com>
2023-04-22 09:58:02 -0500

    Update MixinChestBlockEntityRenderer.java

--- src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinChestBlockEntityRenderer.java
+++ src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinChestBlockEntityRenderer.java
@@ -1,30 +1,10 @@
 package com.red.masaadditions.tweakeroo_additions.mixin;
 
 import com.red.masaadditions.tweakeroo_additions.config.ConfigsExtended;
-import com.red.masaadditions.tweakeroo_additions.config.FeatureToggleExtended;
-import com.red.masaadditions.tweakeroo_additions.util.MiscUtils;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.enums.ChestType;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.render.block.entity.ChestBlockEntityRenderer;
-import net.minecraft.client.util.SpriteIdentifier;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ElytraItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.screen.slot.SlotActionType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ChestBlockEntityRenderer.class)
 public class MixinChestBlockEntityRenderer {
